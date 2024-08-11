b6e792b57a4f5b61073a93ff2ddab7006c78d730
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2024-08-11 19:06:14 +0200

    v.1pr

--- index.rb
+++ index.rb
@@ -1,8 +1,5 @@
 name = "Joe"
-
 if name == "Joe"
-    puts "Welcome to Linux App Store! #{name}"
+    puts "Welcome to Linux App Store #{name}!"      
     puts "Here you can see every app for Linux!"
-else 
-    puts " i don't know you"
 end
