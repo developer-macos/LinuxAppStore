ebdda271bb0a835d5a5e12654421ddbab7abe680
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2024-09-01 11:21:54 +0200

    preview

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
-// window.addEventListener("load", () => {
-//   const loader = document.querySelector(".loader");
-
-//   loader.classList.add("loader--hidden");
-
-//   loader.addEventListener("transitionend", () => {
-//     document.body.removeChild(loader);
-//   });
-// });
-
+var name = prompt("What's your name?");
+alert("Hello " + name);
+var likesCats = confirm("Do you like Games")
+if (likesCats) {
+  alert("Me too!")
+}
+else {
+  alert("I am sorry. But you are nor my friend.")
+}
